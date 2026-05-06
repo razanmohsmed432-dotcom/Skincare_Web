c862db4c328ae8758613d9d13ba02ee17e48919b
shahd <shadaljak2@gmail.com>
2026-05-06 17:24:51 +0300

    delete

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $products = $product->readAll();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>متجر العناية بالبشرة</title>
+    <title> عة</title>
     <style>
         * {
             box-sizing: border-box;
